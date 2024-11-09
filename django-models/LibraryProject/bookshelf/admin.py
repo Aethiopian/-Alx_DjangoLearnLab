f09f7f06eebeed3c783787811cd0b70a49d6b799
Dambi <imail.oromo@gmail.com>
2024-11-09 10:52:57 +0300

    Remove django-models directory

--- django-models/LibraryProject/bookshelf/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # Display these fields in the admin list view
-    list_filter = ('publication_year',)  # Filter by publication year
-    search_fields = ('title', 'author')  # Add search capabilities by title and author
