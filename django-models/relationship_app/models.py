ec70c0a4452a71e7387a16c34294eda127e00ea7
Dambi <imail.oromo@gmail.com>
2024-11-09 15:13:25 +0300

    Fifth commit on relationship_app

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,40 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
-
-from django.db import models
-
-# Author Model
 class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
+	name = models.CharField(max_length=100)
+	def __str__(self):
         return self.name
 
-
-# Book Model
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+	title = models.CharField(max_length=100)
+	author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+	 def __str__(self):
+        return self.title
+
+class Library(models.Model):
+	name = models.CharField(max_length=100)
+	books = models.ManyToManyField(Book, related_name='Library')
+	def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+	name = models.CharField(max_length=100)
+	library = models.OneToOneField(Library, on_delete=models.CASCADE)
+	def __str__(self):
+        return self.name
+ROLE_CHOICES = (
+    ('Admin', 'Admin'),
+    ('Librarian', 'Librarian'),
+    ('Member', 'Member'),
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Link to Django's built-in User model
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f'{self.user.username} ({self.role})'
+class Meta:
+        permissions = [
+            ("can_add_book", "Can add a book"),
+            ("can_change_book", "Can change a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
 
     def __str__(self):
         return self.title
-
-
-# Library Model
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book, related_name='libraries')
-
-    def __str__(self):
-        return self.name
-
-
-# Librarian Model
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
-
-    def __str__(self):
-        return self.name
-
