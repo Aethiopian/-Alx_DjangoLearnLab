b2a501395c20266aa01f6c23d74d2f6d3e364766
Dambi <imail.oromo@gmail.com>
2024-12-15 13:11:23 +0300

    Initial Setup and Project Configuration for a Django Blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,32 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils.text import slugify
 from taggit.managers import TaggableManager
+from django.utils.timezone import now
+
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    tags = TaggableManager()
+    tags = TaggableManager()  # Add tagging functionality
+    published_date = models.DateTimeField(default=now, blank=True, null=True)
 
     def __str__(self):
         return self.title
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True)
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
 
 class Comment(models.Model):
-    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"Comment by {self.author} on {self.post.title}"
+        return f'Comment by {self.author} on {self.post.title}'
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.post.pk})
